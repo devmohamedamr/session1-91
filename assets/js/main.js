89b5b0b5e19973e6492fb69fef476b21c52a7726
mohamed amr <dev.mohamedamr@gmail.com>
2023-08-29 21:13:20 +0300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+// ================================================================
+// var btn  = document.getElementById("btn")
+// var password = document.getElementById("password")
+// btn.onclick = function(){
+//     if(password.type == "password"){
+//         password.type = "text"
+//         btn.innerText = "hide"
+//     }else {
+//         password.type = "password"
+//         btn.innerText = "show"
+//     }
+// }
+// ================================================================
+
+// var text = document.getElementById("text")
+// var res = document.getElementById("res")
+// text.onkeyup = function(){
+//     var count = text.value.length
+//     if(count >= 20){
+//         text.style.cssText = "border:1px solid red;outline:1px solid red"
+//     }else{ 
+//         text.style.cssText = "border:1px solid black;outline:1px solid black"
+//     }
+//     res.innerText = "count is : "+count
+// }
+// ================================================================
+// var save = document.getElementById("save")
+// var nameInput = document.getElementById("name")
+// var username = document.getElementById("username")
+// var nameres = document.getElementById("nameres")
+// var usernameres = document.getElementById("usernameres")
+// save.onclick = function(){
+//     if(nameInput.value.length <= 0){
+//         nameInput.style.cssText = "border:1px solid red;outline:1px solid red"
+//         nameres.innerText = "name is required"
+//     }else{
+//         nameInput.style.cssText = "border:1px solid black;outline:1px solid black"
+//         nameres.innerText = ""
+//     }
+//     if(username.value.length <= 0){
+//         username.style.cssText = "border:1px solid red;outline:1px solid red"
+//         usernameres.innerText = "username is required"
+
+//     }else{
+//         username.style.cssText = "border:1px solid black;outline:1px solid black"
+//         usernameres.innerText = ""
+//     }
+// }
+
+
+// console.log(document.getElementById("username").value)
+// console.log( $("#username").val() )
